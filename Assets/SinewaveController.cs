5423bbadb3c830a0ae2d86df67c7f7c1ff6dbe0c
Kelly MacNeill <pyromuffin@gmail.com>
2014-05-31 13:12:58 -0400

    sixsense and vr integrated

---
+++ Assets/SinewaveController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SinewaveController : MonoBehaviour {
+
+    public float myAmplitude;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.one *Mathf.Sin(Time.timeSinceLevelLoad) * myAmplitude;
+	}
+}
