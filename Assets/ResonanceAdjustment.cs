c78b8d8baf981756b2e6dac2b02db8aa7db5e259
markstock <markjstock@gmail.com>
2014-06-01 12:55:14 -0400

    Faster swimming stiffens spring and increases resonant frequency

---
+++ Assets/ResonanceAdjustment.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResonanceAdjustment : MonoBehaviour {
+
+	public GameObject velController;
+	public ConfigurableJoint mainJoint;
+	public float minJointXYspring;
+	public float jointXYspringScale;
+
+	// Use this for initialization
+	void Start () {
+		//mainJoint.angularYZDrive.positionSpring = minJointXYspring;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float velMag = velController.rigidbody.velocity.magnitude;
+		var drive = mainJoint.angularYZDrive;
+
+		drive.positionSpring = minJointXYspring + velMag*jointXYspringScale;
+			mainJoint.angularYZDrive = drive;//   .positionSpring = minJointXYspring + velMag*jointXYspringScale;
+	}
+}
